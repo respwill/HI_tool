42bb68474abc9bacf64393776e9c4c9237a933ea
respwill <gonchogo@hotmail.com>
2017-02-01 13:20:32 +0900

    change line to ignore no meaningful data

--- CES_read.py
+++ CES_read.py
@@ -27,6 +27,8 @@ class CES_reader():
                 continue
             elif self.CES_df[self.lot_column].loc[n] == self.lot_column:
                 continue
+            elif self.CES_df[self.lot_column].loc[n].find("복사") != -1:
+                continue
             else:
                 if self.dcc_column in self.CES_df:
                     self.CES_df[self.dcc_column] = self.CES_df[self.dcc_column].fillna('')
